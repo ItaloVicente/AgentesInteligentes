c25b21e23d2ae7e22161902b8dcf226a1ee5a425
Maria de Lourdes Melo Linhares de Brito <malulinharess@gmail.com>
2024-12-02 17:57:40 -0300

    WIP

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
         int iteracoes = 10; // Número de execuções
         List<Integer> listaNumSujeiras = new ArrayList<>();
